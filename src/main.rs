d534f04c53a079a12038a72c28a02a6818302ed7
kemoshumai <87066961+kemoshumai@users.noreply.github.com>
2024-06-26 13:08:48 +0900

    キーとスケールの選択画面を作成

---
+++ src/main.rs
@@ -0,0 +1,116 @@
+use leptos::*;
+use web_sys::HtmlDivElement;
+fn main() {
+    mount_to_body(|| view! { <App /> } );
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum Scale {
+    Major,
+    Minor,
+}
+
+#[component]
+fn App() -> impl IntoView {
+
+    let (key_note_number, set_key_note_number) = create_signal(Some(60));
+    let (scale, set_scale) = create_signal(Scale::Major);
+
+    let horizontal_scroll_handler = |event: leptos::ev::WheelEvent| {
+
+        // マウスパッドでの横スクロールを許容する
+        if event.delta_y().abs() < event.delta_x().abs() {
+            return;
+        }
+    
+        let target: HtmlDivElement = event_target(&event);
+        target.set_scroll_left(target.scroll_left() + event.delta_y() as i32);
+        
+    };
+
+    view! {
+        <div class="m-5">
+            <header>
+                <p class="text-8xl">{"Seorist"}</p>
+                <div class="flex gap-2 overflow-x-scroll cursor-pointer py-3" on:wheel=horizontal_scroll_handler>               <For 
+                        each = move || {0..=127-12}
+                        key = |&i|{i}
+                        children = { move |i| {
+                            let note_name = get_note_name(i);
+                            let label = format!("{}{}", note_name, i as i8/12-1);
+                            view! {
+                                <Card label={label} handler_on_click={move|_|{set_key_note_number.update(|j|*j=Some(i))}} />
+                            }
+                        }}
+                    />
+                </div>
+            </header>
+            <main class="flex flex-col items-center">
+                {
+                    move || key_note_number.get().is_some().then(|| {
+                        let key_note_number = key_note_number.get().unwrap();
+
+                        let major_span_class = format!(
+                            "px-2 cursor-pointer {}",
+                            if scale.get() == Scale::Major {"font-medium"} else { "" }
+                        );
+
+                        let minor_span_class = format!(
+                            "px-2 cursor-pointer {}",
+                            if scale.get() == Scale::Minor {"font-medium"} else { "" }
+                        );
+                        
+                        view! {
+                            <h2>{"Choose the scale."}</h2>
+                            <p class="my-4">
+                                <span class={major_span_class} on:click=move|_|set_scale.set(Scale::Major) >major</span>
+                                <span>{"/"}</span>
+                                <span class={minor_span_class} on:click=move|_|set_scale.set(Scale::Minor) >minor</span>
+                            </p>
+                            <div class="flex gap-2">
+                                <For 
+                                    each=||{0..12}
+                                    key=|&i|{i}
+                                    children=move|i|{
+                                        let note_name = get_note_name(key_note_number+i);
+                                        let label = format!("{}{}", note_name, (key_note_number+i) as i8/12-1);
+                                        view! {
+                                            <Card label={label} handler_on_click={|_|{}} />
+                                        }
+                                    }
+                                />
+                            </div>
+                        }
+                    })
+                }
+            </main>
+        </div>
+    }
+}
+
+#[component]
+fn Card(label: String, handler_on_click: impl Fn(leptos::ev::MouseEvent) + 'static ) -> impl IntoView {
+    view! {
+        <div class="block bg-white shadow-md rounded-lg h-24 aspect-[3/4] flex items-center justify-center" on:click=handler_on_click>
+            <p class="text-2xl h-min w-min">{label}</p>
+        </div>
+    }
+}
+
+fn get_note_name(key_number: u8) -> &'static str{
+    match key_number%12 {
+        0 => "C",
+        1 => "C#",
+        2 => "D",
+        3 => "D#",
+        4 => "E",
+        5 => "F",
+        6 => "F#",
+        7 => "G",
+        8 => "G#",
+        9 => "A",
+        10 => "A#",
+        11 => "B",
+        _ => panic!("Invalid key number"),
+    }
+}
